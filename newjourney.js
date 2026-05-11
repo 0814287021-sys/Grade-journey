7723b67c99c7357ed8393f308a6a473331f3f6a4
0814287021-sys <0814287021@s.tusd1.org>
2026-05-11 15:49:45 +0000

    05/11

--- newjourney.js
+++ newjourney.js
@@ -1,13 +1,1 @@
-function submitGrade() {
-    let gradeValue = document.getElementById("gradeInput").value;
-    
-    if (gradeValue) {
-        alert("Grade saved: " + gradeValue);
-        // You can add more code here later
-    } else {
-        alert("Please enter a grade!");
-    }
-}
 
-let submitButton = document.getElementById("submitGradeButton");
-submitButton.addEventListener("click", submitGrade);
